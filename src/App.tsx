156a25926dc2634eda99b6e7f3e48132eabf19eb
khthondev <khthondev@gmail.com>
2024-12-20 13:58:18 +0700

    Updated deployment workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <HashRouter>
         <div className="min-h-screen flex flex-col">
           <Header />
           <main className="flex-grow pt-20">
@@ -32,7 +32,7 @@ const App = () => (
           </main>
           <Footer />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+/* import path from "path";
+import { componentTagger } from "lovable-tagger"; */
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+/* export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
@@ -19,4 +19,9 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})); */
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/valkyra/',
+})
